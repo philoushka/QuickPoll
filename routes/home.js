d336c8efbf4a71b414b2a617d91fa577ba92cf90
Phil <philoushka@gmail.com>
2014-07-06 05:24:01 +0000

    add admin page.

--- routes/home.js
+++ routes/home.js
@@ -5,4 +5,7 @@ module.exports = function (app) {
     app.get('/about', function (req, res) {
         res.render('about');
     });
+    app.get('/admin', function (req, res) {
+        res.render('tally/admin');
+    });
 }
